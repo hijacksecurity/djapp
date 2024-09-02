495dc5cd8cb4df00d19004288999d33c430c9cfb
Hijack Security <hijacksecurity@protonmail.com>
2024-09-01 20:27:57 -0400

    entrypoint.sh and data folder for non-prod data

--- djapp/models.py
+++ djapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class TestModel(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    age = models.IntegerField(null=True, blank=True)  # New field added
 
     def __str__(self):
         return self.name
--- djapp/views.py
+++ djapp/views.py
@@ -1,19 +1,18 @@
+import os
+
 from django.http import HttpResponse
 from .models import TestModel
 
 
 def index(request):
-    # Fetch all records from the TestModel
     items = TestModel.objects.all()
 
-    # Create a simple HTML response
-    response_html = "<h1>Welcome to DJApp!</h1><p>Your Django server is running correctly.</p>"
-    response_html += "<h2>Items in the Database:</h2><ul>"
+    # Simple welcome message
+    response_html = "<h2>Welcome to the Test Page!</h2><p>This is <b>{}</b> environment.</p>".format(os.getenv("DJANGO_SETTINGS_MODULE"))
 
-    # Loop through the items and add them to the HTML response
+    response_html += "<ul>"
     for item in items:
-        response_html += f"<li>{item.name}</li>"
-
+        response_html += f"<li>{item.name} - {item.description} - Age: {item.age}</li>"
     response_html += "</ul>"
 
     return HttpResponse(response_html)
