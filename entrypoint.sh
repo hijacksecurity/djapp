86b500ede7ae21395bfe9d7db789b317641df00f
Hijack Security <hijacksecurity@protonmail.com>
2024-09-01 21:47:14 -0400

    entrypoint.sh fix to use env variable for host

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,15 @@ set -e
 
 # Function to wait for the database to be ready
 wait_for_db() {
-  echo "Waiting for database connection..."
-  until python manage.py check --database default; do
+  # Determine which database to check based on DB_HOST environment variable
+  if [ -n "$DB_HOST" ]; then
+    DB_TO_CHECK=$DB_HOST
+  else
+    DB_TO_CHECK=default
+  fi
+
+  echo "Waiting for database connection ($DB_TO_CHECK)..."
+  until python manage.py check --database $DB_TO_CHECK; do
     >&2 echo "Database is unavailable - sleeping"
     sleep 1
   done
